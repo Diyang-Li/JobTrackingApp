ffd5330568f358762b99cfd2709d4ea7c376ddbb
Li, Diyang <diyangli233@gmail.com>
2023-08-14 22:03:28 -0400

    test restful of get and create job

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(5100, () => {
-  console.log("server is running....");
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({
+    message: "data received",
+    data: req.body,
+  });
+});
+// Get All Jobs
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+// Create Jobs
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  } else {
+    const id = nanoid(10);
+    const job = { id, company, position };
+    jobs.push(job);
+    res.status(200).json({ jobs });
+  }
+});
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server is running on PORT ${port}....`);
 });
