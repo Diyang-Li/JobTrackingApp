20d6e88fc1f52daaeab7302f695a124102070415
Li, Diyang <diyangli233@gmail.com>
2023-08-15 10:27:43 -0400

    complete edit and delete job resful

--- server.js
+++ server.js
@@ -44,6 +44,43 @@ app.post("/api/v1/jobs", (req, res) => {
     res.status(200).json({ jobs });
   }
 });
+// GET single job
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(400).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+// Edit job
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(400).json({ msg: `no job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job has modified", job });
+});
+
+// DELETE single job
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(400).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id === id);
+  jobs = newJobs;
+  res.status(200).json({ mag: "job has deleted", jobs });
+});
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
