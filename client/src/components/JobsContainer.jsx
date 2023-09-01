417fb3a1534eec6a98257030933650fa7b607f68
Li, Diyang <diyangli233@gmail.com>
2023-09-01 18:26:15 -0400

    complete the pagenation function

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,10 +1,12 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
+import PageBtnContainer from "./PageBtnContainer";
 import { userAllJobsContext } from "../pages/AllJobs";
 
 const JobsContainer = () => {
   const { data } = userAllJobsContext();
-  const { jobs } = data;
+  console.log(data);
+  const { jobs, totalJobs, numOfPages } = data;
 
   if (jobs.length == 0) {
     return (
@@ -15,11 +17,15 @@ const JobsContainer = () => {
   }
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
