91a819e68b1e12d99f07afbe03d1f0981e1fffad
Li, Diyang <diyangli233@gmail.com>
2023-09-01 11:45:23 -0400

    pagenation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,8 @@ import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as adminLoader } from "./pages/Admin";
 import { action as profileAction } from "./pages/Profile";
+import { loader as statsLoader } from "./pages/Stats";
+
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
   document.body.classList.toggle("dark-theme", isDarkTheme);
@@ -74,6 +76,7 @@ const router = createBrowserRouter([
       {
         path: "stats",
         element: <Stats />,
+        loader: statsLoader,
       },
       {
         path: "all-jobs",
